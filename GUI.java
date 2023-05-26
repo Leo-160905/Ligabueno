8a55c3d24d496883bba7fe212a5ee3fe44fe1d80
Leo-160905 <Leonardvonfircks@gmail.com>
2023-05-26 11:06:37 +0200

    improvements

--- GUI.java
+++ GUI.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+
 public class GUI extends JFrame {
 
 
@@ -9,173 +10,154 @@ public class GUI extends JFrame {
     public JButton bSETTINGS = new JButton();
     public JButton bQUIT = new JButton();
     public JButton bRESUME = new JButton();
+    Timer timer = new Timer(1000, null);
+    Dimension frameSize;
+    public static int sek = 0;
+    public Panel jpanel;
 
-      Panel panel;
+    public GUI() {
 
-    private Listener listener;
+        super();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        frameSize = new Dimension(d.width / 2, d.height / 2);
+        jpanel = new Panel(frameSize);
+        jpanel.setVisible(false);
+        add(jpanel);
+        setSize(frameSize);
 
-    public GUI(){
+        Point framePosition = new Point(480, 270);
+        setLocation(framePosition);
+        setLayout(null);
+        setTitle("GUI");
+        setResizable(false);
+        //--------------------------------------------------------
+        int Startwidth = 200;
+        int Startheight = 100;
+        int Startx = (frameSize.width / 2) - (Startwidth / 2);
+        int Starty = (frameSize.height / 2) - (Startheight * 2);
+        bSTART.setText("START");
+        bSTART.setBounds(Startx, Starty, Startwidth, Startheight);
+        bSTART.setForeground(Color.BLACK);
+        bSTART.setVisible(true);
+        bSTART.setFocusable(false);
+        bSTART.addActionListener((e) -> {
+            jpanel.setVisible(true);
+            // listener.timer.start();
+            bSTART.setVisible(false);
+            bSETTINGS.setVisible(false);
+            bQUIT.setVisible(false);
+            timer.start();
+        });
 
-    super();
-    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-    int framewidth = d.width/2;
-    int frameheight = d.height/2;
-    setSize(framewidth, frameheight);
+        add(bSTART);
 
-    int x = 480;
-    int y = 270;
-    setLocation(x,y);
-    setLayout(null);
-    setTitle("GUI");
-    setResizable(false);
+        //--------------------------------------------------------
+        bRESUME.addActionListener((e) -> {
+            bRESUME.setVisible(false);
+            bQUIT.setVisible(false);
+            bSETTINGS.setVisible(false);
+            jpanel.setVisible(true);
+            sek = 0;
+            timer.start();
+        });
+        int Resumewidth = 200;
+        int Resumeheight = 100;
+        int Resumex = (frameSize.width / 2) - (Resumewidth / 2);
+        int Resumey = (frameSize.height / 2) - (Resumeheight * 2);
+        bRESUME.setText("RESUME");
+        bRESUME.setBounds(Resumex, Resumey, Resumewidth, Resumeheight);
+        bRESUME.setForeground(Color.BLACK);
+        bRESUME.setVisible(false);
+        bRESUME.setFocusable(false);
+        add(bRESUME);
+        //--------------------------------------------------------
 
-//--------------------------------------------------------
-    bSTART.addActionListener(new ActionListener() {
+        bBACK.addActionListener((e) -> {
+            bQUIT.setVisible(true);
+            bSETTINGS.setVisible(true);
+            bBACK.setVisible(false);
+            bRESUME.setVisible(true);
+            bSTART.setVisible(false);
+            jpanel.setVisible(false);
+            timer.stop();
+        });
+        int Backwidth = 100;
+        int Backheight = 100;
+        int Backx = (frameSize.width / 12) - (Backwidth / 2);
+        int Backy = frameSize.height - (frameSize.height / 4) - (Backheight / 3);
+        bBACK.setText("BACK");
+        bBACK.setBounds(Backx, Backy, Backwidth, Backheight);
+        bBACK.setForeground(Color.BLACK);
+        bBACK.setVisible(false);
+        bBACK.setFocusable(false);
+        add(bBACK);
+        //--------------------------------------------------------
 
-        public void actionPerformed(ActionEvent e) {
-            bSTART_actionPerformed(e);
-        }
-    });
-    int Startwidth = 200;
-    int Startheight = 100;
-    int Startx = (framewidth/2)-(Startwidth/2);
-    int Starty = (frameheight/2)-(Startheight*2);
-    bSTART.setText("START");
-    bSTART.setBounds(Startx,Starty,Startwidth,Startheight);
-    bSTART.setForeground(Color.BLACK);
-    bSTART.setVisible(true);
-    add(bSTART);
+        bSETTINGS.addActionListener((e) -> {
+            bRESUME.setVisible(false);
+            bQUIT.setVisible(false);
+            bSETTINGS.setVisible(false);                                //Button Settings
+            bBACK.setVisible(true);
+            bSTART.setVisible(false);
+        });
+        int Setwidth = 200;
+        int Setheight = 100;
+        int Setx = (frameSize.width / 2) - (Setwidth / 2);
+        int Sety = (frameSize.height / 2) - (Setheight / 2) - (Setheight / 4);
+        bSETTINGS.setText("SETTINGS");
+        bSETTINGS.setBounds(Setx, Sety, Setwidth, Setheight);
+        bSETTINGS.setForeground(Color.BLACK);
+        bSETTINGS.setVisible(true);
+        bSETTINGS.setFocusable(false);
+        add(bSETTINGS);
+        //--------------------------------------------------------
+        bQUIT.addActionListener((e) -> System.exit(0));
+        int Quitwidth = 200;
+        int Quitheight = 100;
+        int Quitx = (frameSize.width / 2) - (Setwidth / 2);
+        int Quity = (frameSize.height / 2) + (Quitheight / 2);
+        bQUIT.setText("QUIT");
+        bQUIT.setBounds(Quitx, Quity, Quitwidth, Quitheight);
+        bQUIT.setForeground(Color.BLACK);
+        bQUIT.setVisible(true);
+        bQUIT.setFocusable(false);
+        add(bQUIT);
 
-//--------------------------------------------------------
-    bRESUME.addActionListener(new ActionListener() {
-
-        public void actionPerformed(ActionEvent e) {
-            bRESUME_actionPerformed(e);
-        }
-    });
-    int Resumewidth = 200;
-    int Resumeheight = 100;
-    int Resumex = (framewidth/2)-(Resumewidth/2);
-    int Resumey = (frameheight/2)-(Resumeheight*2);
-    bRESUME.setText("RESUME");
-    bRESUME.setBounds(Resumex, Resumey, Resumewidth, Resumeheight);
-    bRESUME.setForeground(Color.BLACK);
-    bRESUME.setVisible(false);
-    add(bRESUME);
-//--------------------------------------------------------
-
-    bBACK.addActionListener(new ActionListener() {
-
-        public void actionPerformed(ActionEvent e) {
-            bBACK_actionPerformed(e);
-        }
-    });
-    int Backwidth = 100;
-    int Backheight = 100;
-    int Backx = (framewidth/12)-(Backwidth/2);
-    int Backy = frameheight-(frameheight/4)-(Backheight/3);
-    bBACK.setText("BACK");
-    bBACK.setBounds(Backx,Backy,Backwidth,Backheight);
-    bBACK.setForeground(Color.BLACK);
-    bBACK.setVisible(true);
-    add(bBACK);
-//--------------------------------------------------------
-
-    bSETTINGS.addActionListener(new ActionListener() {
-
-        public void actionPerformed(ActionEvent e) {
-            bSETTINGS_actionPerformed(e);
-        }
-    });
-    int Setwidth = 200;
-    int Setheight = 100;
-    int Setx = (framewidth/2)-(Setwidth/2);
-    int Sety = (frameheight/2)-(Setheight/2)-(Setheight/4);
-    bSETTINGS.setText("SETTINGS");
-    bSETTINGS.setBounds(Setx,Sety,Setwidth,Setheight);
-    bSETTINGS.setForeground(Color.BLACK);
-    bSETTINGS.setVisible(true);
-    add(bSETTINGS);
-//--------------------------------------------------------
-    bQUIT.addActionListener(new ActionListener() {
-
-        public void actionPerformed(ActionEvent e) {
-            bQUIT_actionPErformed(e);
-        }
-    });
-    int Quitwidth = 200;
-    int Quitheight = 100;
-    int Quitx = (framewidth/2)-(Setwidth/2);
-    int Quity = (frameheight/2)+(Quitheight/2);
-    bQUIT.setText("QUIT");
-    bQUIT.setBounds(Quitx,Quity,Quitwidth,Quitheight);
-    bQUIT.setForeground(Color.BLACK);
-    bQUIT.setVisible(true);
-    add(bQUIT);
-
-//--------------------------------------------------------
+        //--------------------------------------------------------
 
         //System.out.println(Startx+"|"+Starty);
 
+        //--------------------------------------------------------
 
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE && jpanel.isVisible()) {
+                    timer.stop();
+                    jpanel.setVisible(false);
+                    bRESUME.setVisible(true);
+                    bSETTINGS.setVisible(true);
+                    bQUIT.setVisible(true);
+                    sek = 0;
+                }
+            }
+        });
+        //--------------------------------------------------------
 
-    setVisible(true);
-    new Panel(this);
-    new Listener(this);
-
-
-    }
-
-
-    public void bSTART_actionPerformed(ActionEvent e){
-
-
-        panel.jpanel.setVisible(true);
-       // listener.timer.start();
-        bSTART.setVisible(false);
-        bSETTINGS.setVisible(false);
-        bQUIT.setVisible(false);
-
-
-
-    }
-    public void bRESUME_actionPerformed(ActionEvent e){
-        bRESUME.setVisible(false);
-        bQUIT.setVisible(false);
-        bSETTINGS.setVisible(false);
-        panel.jpanel.setVisible(true);
-        listener.timer.start();
-    }
-    public void bBACK_actionPerformed(ActionEvent e){
-       /* bQUIT.setVisible(true);
-        bSETTINGS.setVisible(true);
-        bBACK.setVisible(false);
-        // Wenn der Button back bei 0sek gedrückt wird,
-        if (listener.sek == 0) {                                                    //wird der Button Start sichtbar wenn es keine 0 sek sind,
-            bSTART.setVisible(true);                                         // wird der Button Resume sichtbar
-        } else if (0 <= listener.sek) {
-
-            bSTART.setVisible(false);
-            bRESUME.setVisible(true);
-
-        } else {
-            bRESUME.setVisible(true);
-        }*/
-
-    }
-    public void bSETTINGS_actionPerformed(ActionEvent e){
-        bRESUME.setVisible(false);
-        bQUIT.setVisible(false);
-        bSETTINGS.setVisible(false);                                //Button Settings
-        bBACK.setVisible(true);
-        bSTART.setVisible(false);
-
-
-
-
-    }
-    public void bQUIT_actionPErformed(ActionEvent e){
-    dispose();
+        timer.addActionListener((e) -> {
+            sek++;
+            System.out.println(sek);
+            if (sek == 11) {
+                timer.stop();
+                jpanel.setVisible(false);
+                bSTART.setVisible(true);
+                bSETTINGS.setVisible(true);
+                bQUIT.setVisible(true);
+                sek = 0;
+            }
+        });
+        setVisible(true);
     }
 }
--- Panel.java
+++ Panel.java
@@ -1,18 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Panel {
-    public JPanel jpanel = new JPanel();
-
-    public Panel(GUI gui){
-        gui.add(jpanel);
-        jpanel.setBounds(0 ,0,gui.getSize().width, gui.getSize().height);
-        jpanel.setVisible(false);
-        jpanel.setBackground(Color.BLACK);
-        jpanel.setOpaque(true);
+public class Panel extends JPanel{
 
 
-        new Listener(this);
+    public Panel(Dimension d){
+        setBounds(0 ,0,d.width, d.height);
+        setVisible(false);
+        setBackground(Color.BLACK);
+        setOpaque(true);
 
     }
 
